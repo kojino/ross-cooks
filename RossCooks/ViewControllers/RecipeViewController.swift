66da5ec7c0860ab02d4bad952303d1eef2d71813
Luca Hagel <hagel.luca@gmail.com>
2016-10-20 13:05:18 -0700

    Cleanup

--- RossCooks/ViewControllers/RecipeViewController.swift
+++ RossCooks/ViewControllers/RecipeViewController.swift
@@ -55,12 +55,10 @@ class RecipeViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-    
+  
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 2
     }
     
